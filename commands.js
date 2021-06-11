4fc06193ab9a465dc321866f3409565ee0a7e6e2
Brenden2008 <brendenstahle@gmail.com>
2021-06-11 14:58:21 -0400

    2 files modified

--- commands.js
+++ commands.js
@@ -1,9 +1,15 @@
 const commands = {
-    info: (instance) => {
-        instance.output('WebPC running JSOS preRelease-1.0. Terminal powered by Vanilla Terminal.')
-    },
-    
-    echo: (instance, parameters) => {
-        instance.output(parameters[0]);
-    },
+  info: instance => {
+    instance.output(
+      'WebPC running JSOS preRelease-1.0. Terminal powered by Vanilla Terminal.'
+    );
+  },
+
+  echo: (instance, parameters) => {
+    instance.output(parameters[0]);
+  },
+
+  exe: (instance, parameters) => {
+    instance.output(eval(parameters[0]));
+  }
 };
