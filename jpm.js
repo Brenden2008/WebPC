2ea99a9ee6eba9700bb6505f8dddbe7c95b2ea0f
Brenden2008 <brendenstahle@gmail.com>
2021-06-17 12:38:42 -0500

    1 file modified

--- jpm.js
+++ jpm.js
@@ -36,6 +36,34 @@ const jpm = {
       .get('installed')
       .get(pname)
       .put(false);
+  },
+  upload: function(pname, pdesc, pauth, pinstallurl, puninstallurl) {
+    gun
+      .get('jpm')
+      .get('package')
+      .get(pname)
+      .get('desc')
+      .put(pdesc);
+    gun
+      .get('jpm')
+      .get('package')
+      .get(pname)
+      .get('author')
+      .put(pauth);
+    gun
+      .get('jpm')
+      .get('package')
+      .get(pname)
+      .get('url')
+      .get('install')
+      .put(pinstallurl);
+    gun
+      .get('jpm')
+      .get('package')
+      .get(pname)
+      .get('url')
+      .get('uninstall')
+      .put(puninstallurl);
   }
 };
 
@@ -59,7 +87,7 @@ const jpm_commands = {
         break;
       default:
         instance.output(
-          'JSOS Package Manager Version 1.0 for JSOS. Default storage powered by <a href="https://gofile.io">GoFile.io</a>. Developed by <a href="https://github.com/Brenden2008">Brenden2008</a>. Command list: jpm install (package name), jpm uninstall (package name), jpm list [Lists installed packages], jpm upload (package name) (package description) (package author) (main js script url (MUST BE DIRECT LINK!)) or (local file path), jpm info (package name)'
+          'JSOS Package Manager Version 1.0 for JSOS. Developed by <a href="https://github.com/Brenden2008">Brenden2008</a>. Command list: jpm install (package name), jpm uninstall (package name), jpm list [Lists installed packages], jpm upload (package name) (package description) (package author) (install js script url (MUST BE DIRECT LINK!)) (uninstall js script url (MUST BE DIRECT LINK!)), jpm info (package name)'
         );
     }
   }
